108517d1d72e88b277aee57548e5990e9647effe
Richard Herbert <richard@infoweb.co.uk>
2025-10-14 22:23:46 +0100

    Added fixture filters

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,22 @@
+import { DateTime } from "luxon";
+import 'dotenv/config';
+
 export default async function( eleventyConfig ) {
 	eleventyConfig.addPassthroughCopy( './src/assets/**' );
 	eleventyConfig.addPassthroughCopy( './src/.htaccess' );
-};
 
-import 'dotenv/config';
+	eleventyConfig.addFilter( 'upcomingFixtures', function( records ) {
+		const today = DateTime.now().toISODate();
+
+		return records.filter( record => DateTime.fromISO( record.datetime ).toISODate() >= today );
+	} );
+
+	eleventyConfig.addFilter( 'pastFixtures', function( records ) {
+		const today = DateTime.now().toISODate();
+
+		return records.filter( record => DateTime.fromISO( record.datetime ).toISODate() < today );
+	} );
+};
 
 export const config = {
 	dir: {
